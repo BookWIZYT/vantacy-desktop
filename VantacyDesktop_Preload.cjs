e36de1b127e93370d89899d5e57386550b03d54f
BookWIZYT <bookwizardart@gmail.com>
2026-07-22 08:14:38 -0700

    Refactor contextBridge exposure for vantaDesktop

--- VantacyDesktop_Preload.cjs
+++ VantacyDesktop_Preload.cjs
@@ -1,2 +1,11 @@
 const { contextBridge, ipcRenderer } = require("electron");
-contextBridge.exposeInMainWorld("vantaDesktop", { openProject:()=>ipcRenderer.invoke("open-project"), saveFile:(p,c)=>ipcRenderer.invoke("save-file",p,c), runCommand:c=>ipcRenderer.invoke("run-command",c) });
+
+contextBridge.exposeInMainWorld("vantaDesktop", {
+  openProject: () => ipcRenderer.invoke("open-project"),
+
+  saveFile: (filePath, content) =>
+    ipcRenderer.invoke("save-file", filePath, content),
+
+  runCommand: (command) =>
+    ipcRenderer.invoke("run-command", command)
+});
